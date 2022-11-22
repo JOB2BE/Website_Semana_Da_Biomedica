c2c85f7de659f7667c001c537298d00d77b623a2
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2022-11-22 22:25:21 +0000

    added internationalisation

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Center, Heading, Box, Stack } from 'native-base';
+import { Center, Heading, Box, Stack, Image } from 'native-base';
 import AboutUsIcon from '../../assets/images/AboutUsIcon';
 import ScheduleIcon from '../../assets/images/ScheduleIcon';
 import ActivitiesIcon from '../../assets/images/ActivitiesIcon';
@@ -7,6 +7,15 @@ import SpeakersIcon from '../../assets/images/SpeakersIcon';
 import Logo from '../../assets/images/WhiteBGLogo';
 import { StyleSheet } from 'react-native';
 import { Link } from '../../router/index';
+
+import * as Localization from 'expo-localization'; //Internationalisation dependencies
+import { I18n } from 'i18n-js';
+import { en, pt } from '../../utils/supportedLanguages';
+
+var idiom = new I18n();
+idiom.enableFallback = true; //If a key is missing the default language will be chosen for that string in the webpage
+idiom.translations = { en, pt }; // All our languages
+idiom.locale = Localization.locale; // get the device's current language code
 
 const styles = StyleSheet.create({
 	leftContainer: {
@@ -42,30 +51,30 @@ const styles = StyleSheet.create({
 		elevation: 10,
 	},
 });
-const navbarRoutes = [
-	{
-		name: 'Quem Somos',
-		icon: <AboutUsIcon />,
-		route: '/AboutUs',
-	},
-	{
-		name: 'Horários',
-		icon: <ScheduleIcon />,
-		route: '/Schedule',
-	},
-	{
-		name: 'Actividades',
-		icon: <ActivitiesIcon />,
-		route: '/Activities',
-	},
-	{
-		name: 'Convidados',
-		icon: <SpeakersIcon />,
-		route: '/Speakers',
-	},
-];
 
 export const Navbar = () => {
+	const navbarRoutes = [
+		{
+			name: idiom.t(['navbar', 'aboutUs']),
+			icon: <AboutUsIcon />,
+			route: '/AboutUs',
+		},
+		{
+			name: idiom.t(['navbar', 'schedule']),
+			icon: <ScheduleIcon />,
+			route: '/Schedule',
+		},
+		{
+			name: idiom.t(['navbar', 'activities']),
+			icon: <ActivitiesIcon />,
+			route: '/Activities',
+		},
+		{
+			name: idiom.t(['navbar', 'speakers']),
+			icon: <SpeakersIcon />,
+			route: '/Speakers',
+		},
+	];
 	return (
 		<>
 			<Center>
---
+++ frontend/src/utils/supportedLanguages.js
@@ -0,0 +1,32 @@
+export const en = {
+	navbar: {
+		aboutUs: 'About Us',
+		schedule: 'Schedule',
+		activities: 'Activities',
+		speakers: 'Speakers',
+	},
+	aboutUs: {},
+	landing: {},
+	schedule: {},
+	activities: {},
+	activity: {},
+	speakers: {},
+	speaker: {},
+	feedback: {},
+};
+export const pt = {
+	navbar: {
+		aboutUs: 'Quem Somos',
+		schedule: 'Horários',
+		activities: 'Actividades',
+		speakers: 'Convidados',
+	},
+	aboutUs: {},
+	landing: {},
+	schedule: {},
+	activities: {},
+	activity: {},
+	speakers: {},
+	speaker: {},
+	feedback: {},
+};
