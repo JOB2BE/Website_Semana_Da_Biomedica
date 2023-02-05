0dcd6077619aef767cac79f2e34089b63c57cb8e
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-02-05 14:13:20 +0000

    Updated Projec'st ReadME and Structure

--- frontend/src/pages/FeedbackPage.js
+++ frontend/src/pages/FeedbackPage.js
@@ -4,24 +4,26 @@ import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Button, Text, View } from 'native-base';
 
-export default function LandingPage() {
+export default function FeedBackPage() {
+	const styles = StyleSheet.create({
+		container: {
+			flex: 1,
+			backgroundColor: '#fff',
+			alignItems: 'center',
+			justifyContent: 'center',
+		},
+	});
+
 	const [counter, setCounter] = useState(0);
 
 	return (
 		<View style={styles.container}>
 			<Text>This is the Feedback Page!</Text>
-			<Button variant='alternating' onPress={() => setCounter(counter + 1)}>Add +1 to counter</Button>
+			<Button variant='alternating' onPress={() => setCounter(counter + 1)}>
+				Add +1 to counter
+			</Button>
 			<Text>You have pressed the button {counter} times</Text>
 			<StatusBar style='auto' />
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
