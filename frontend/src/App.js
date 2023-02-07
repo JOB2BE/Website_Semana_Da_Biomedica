3cdab0f24dd418cbc0843d7702eb359b4bde7ef8
Maria Pereira <maria.galhardas@gmail.com>
2023-02-07 00:13:02 +0000

    some changes on partnershipsPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import ActivitiesPage from './pages/ActivitiesPage';
 import SpeakersPage from './pages/SpeakersPage';
 import SpeakerPage from './pages/SpeakerPage';
 import FeedbackPage from './pages/FeedbackPage';
+import PartnershipsPage from './pages/PartnershipsPage';
 import theme from './theme';
 import { useFonts } from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -59,6 +60,7 @@ export default function App() {
 							<Route path='/Speakers' element={<SpeakersPage idiom={i18n} />} />
 							<Route path='/Speaker' element={<SpeakerPage idiom={i18n} />} />
 							<Route path='/Feedback' element={<FeedbackPage idiom={i18n} />} />
+							<Route path='/Partnerships' element={<PartnershipsPage idiom={i18n} />} />
 						</Routes>
 					</Router>
 				</NativeBaseProvider>
