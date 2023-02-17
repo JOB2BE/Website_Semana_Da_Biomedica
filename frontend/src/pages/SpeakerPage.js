55ef507e3dd1dbe40e8a640a0dd42a550fb3d0a9
Maria Pereira <maria.galhardas@gmail.com>
2023-02-17 19:39:21 +0000

    iniciar a SpeakerPage

--- frontend/src/pages/SpeakerPage.js
+++ frontend/src/pages/SpeakerPage.js
@@ -2,25 +2,38 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import { Button, Text, View } from 'native-base';
+import { Button, Column, Row, Text, View } from 'native-base';
+import extendTheme from '../theme';
 
 export default function SpeakerPage() {
 	const styles = StyleSheet.create({
 		container: {
 			flex: 1,
-
 			alignItems: 'center',
 			justifyContent: 'center',
 		},
+
+		RectangleShapeView: {
+			width: 260,
+			height: 449.42,
+			borderRadius: 30,
+			backgroundColor: extendTheme.colors['cream'],
+			marginTop: 20,
+			marginBottom: 20,
+		},
 	});
 
-	const [counter, setCounter] = useState(0);
+	
 	return (
-		<View style={styles.container}>
-			<Text>This is the Speaker Page!</Text>
-			<Button onPress={() => setCounter(counter + 1)}>Add +1 to counter</Button>
-			<Text>You have pressed the button {counter} times</Text>
-			<StatusBar style='auto' />
-		</View>
+		<Column flex={1} space={12}>
+			<Row justifyContent={'center'} style={styles.container}>
+				<View style={styles.RectangleShapeView}>
+
+				</View>
+			</Row>
+		</Column>
+
+		
+		
 	);
 }
