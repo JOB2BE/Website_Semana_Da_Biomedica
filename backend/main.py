b3aa2fc8950205f0c64d4cf2795345b2a440e70d
Miguel Dinis de Sousa <miguel.dinis.s@tecnico.ulisboa.pt>
2023-02-18 16:10:28 +0000

    Add DELETE methods

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import User, Speaker
+from uuid import UUID
 
 app = FastAPI()
 
@@ -17,12 +18,24 @@ def root():
 async def fetchUsers():
     return ## Retreive all speakers from db
 
-
+## Add new users
 @app.post('/api/users')
 async def registerUser(user:User):
     ## Add to db
     return {'id': user.id}
 
+## Delete user
+@app.delete("/api/users/{userID}")
+async def deleteUser(userID: UUID):
+    # for user in db:
+    #     if user.id == userID:
+    #         db.remove(user)
+    #         return
+    raise HTTPException(
+        status_code= 404,
+        detail=f"User with the id:{userID} does not exist"
+    ) #raise exceptions
+            
 
 ## Show all speakers
 
@@ -30,12 +43,25 @@ async def registerUser(user:User):
 async def fetchSpeakers():
     return ## Retreive all speakers from db
 
-
+## Add new speakers
 @app.post('/api/speakers')
 async def registerSpeaker(speaker:Speaker):
     ## Add to db
     return {'id': speaker.name}
 
+## Delete speaker
+@app.delete("/api/speakers/{name}")
+async def deleteUser(name: str):
+    # for speaker in db:
+    #     if speakr.name == name:
+    #         db.remove(speaker)
+    #         return
+
+    raise HTTPException(
+        status_code= 404,
+        detail=f"Speaker with the name:{name} does not exist"
+    ) #raise exceptions
+        
 
 
 
