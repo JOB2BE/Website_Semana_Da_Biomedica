ff32712564ab3bc4140ee8d39f9438b133b02444
David Fontoura <45210452+DavidFontoura@users.noreply.github.com>
2023-02-24 18:22:25 +0000

    Added visual confirmation of successful registration. Added button to redirect to login page or home page after successful registration

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -12,6 +12,8 @@ import {
 	Column,
 	Row,
 	HStack,
+	Box,
+	Heading,
 } from 'native-base';
 import { useState } from 'react';
 import StyledBox from '../components/information/StyledBox';
@@ -24,11 +26,12 @@ import { Platform } from 'react-native';
 
 export default function RegisterPage() {
 	const [regData, setData] = useState('');
-	const [errors, setErrors] = React.useState({});
+	const [errors, setErrors] = useState({});
+	const [validated, setValidated] = useState(false);
 
 	// For showing and hiding password text
-	const [show1, setShow1] = React.useState(false);
-	const [show2, setShow2] = React.useState(false);
+	const [show1, setShow1] = useState(false);
+	const [show2, setShow2] = useState(false);
 
 	//   Regex represent parameters we *want*.
 	const regexPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/; // Only numbers or letters. No spaces
@@ -45,13 +48,13 @@ export default function RegisterPage() {
 	const validate = () => {
 		let newErrors = {};
 
-		if (regData.name !== undefined) {
+		if (regData.name) {
 			// TODO: PROFANITY CHECK
 		} else {
 			newErrors = { ...newErrors, name: 'Please provide us your name' };
 		}
 
-		if (regData.email !== undefined) {
+		if (regData.email) {
 			if (!regexEmail.test(regData.email)) {
 				newErrors = { ...newErrors, email: 'Please provide us with a valid email' };
 			}
@@ -60,7 +63,7 @@ export default function RegisterPage() {
 		}
 
 		// Password
-		if (regData.password !== undefined) {
+		if (regData.password) {
 			if (
 				regData.password.length < 6 ||
 				regData.password.length > 16 ||
@@ -77,7 +80,7 @@ export default function RegisterPage() {
 		}
 
 		// Confirm password
-		if (regData.password2 !== undefined) {
+		if (regData.password2) {
 			if (regData.password2 !== regData.password) {
 				newErrors = { ...newErrors, password2: 'Passwords do not match!' };
 			}
@@ -86,14 +89,14 @@ export default function RegisterPage() {
 		}
 
 		// Degree
-		if (regData.degree !== undefined) {
+		if (regData.degree) {
 			if (!regexDegree.test(regData.degree)) {
 				newErrors = { ...newErrors, degree: 'Remove any number' };
 			}
 		}
 
 		// University
-		if (regData.university !== undefined) {
+		if (regData.university) {
 			if (!regexUniversity.test(regData.university)) {
 				newErrors = { ...newErrors, university: 'Remove any number' };
 			}
@@ -102,6 +105,7 @@ export default function RegisterPage() {
 		setErrors(newErrors);
 
 		if (isEmptyObj(newErrors)) {
+			setValidated(true);
 			console.log('Success!');
 			// TODO: CODE OF REGISTRATION
 			// handleRegister()
@@ -119,256 +123,310 @@ export default function RegisterPage() {
 					headingText='Register'
 					childrenJustifyContent='center'
 				>
-					<FormControl
-						className={'NameContainer'}
-						isRequired
-						isInvalid={'name' in errors}
-					>
-						<FormControl.Label>
-							<Text size='md'> Name</Text>
-						</FormControl.Label>
+					{!validated ? (
+						<Box className={'NotValidated'}>
+							<FormControl
+								className={'NameContainer'}
+								isRequired
+								isInvalid={'name' in errors}
+							>
+								<FormControl.Label>
+									<Text size='md'> Name</Text>
+								</FormControl.Label>
 
-						<Input
-							variant='filled'
-							rounded='10'
-							_focus={{ bg: white }}
-							onChangeText={(value) => setData({ ...regData, name: value })}
-						/>
+								<Input
+									variant='filled'
+									rounded='10'
+									_focus={{ bg: white }}
+									onChangeText={(value) => setData({ ...regData, name: value })}
+								/>
 
-						{'name' in errors ? (
-							<HStack marginTop='2'>
-								<AntDesign name='exclamationcircleo' size='xs' color={red} />
-								<Text color={red}> {errors.name}</Text>
-							</HStack>
-						) : (
-							<FormControl.HelperText>{null}</FormControl.HelperText>
-						)}
-					</FormControl>
+								{'name' in errors ? (
+									<HStack marginTop='2'>
+										<AntDesign
+											name='exclamationcircleo'
+											size='xs'
+											color={red}
+										/>
+										<Text color={red}> {errors.name}</Text>
+									</HStack>
+								) : (
+									<FormControl.HelperText>{null}</FormControl.HelperText>
+								)}
+							</FormControl>
 
-					<FormControl
-						className={'EmailContainer'}
-						isRequired
-						isInvalid={'email' in errors}
-					>
-						<FormControl.Label>
-							<Text size='md'> Email</Text>
-						</FormControl.Label>
+							<FormControl
+								className={'EmailContainer'}
+								isRequired
+								isInvalid={'email' in errors}
+							>
+								<FormControl.Label>
+									<Text size='md'> Email</Text>
+								</FormControl.Label>
 
-						<Input
-							variant='filled'
-							rounded='10'
-							_focus={{ bg: white }}
-							onChangeText={(value) => setData({ ...regData, email: value })}
-						/>
+								<Input
+									variant='filled'
+									rounded='10'
+									_focus={{ bg: white }}
+									onChangeText={(value) => setData({ ...regData, email: value })}
+								/>
 
-						{'email' in errors ? (
-							<HStack marginTop='2'>
-								<AntDesign name='exclamationcircleo' size='xs' color={red} />
-								<Text color={red}> {errors.email}</Text>
-							</HStack>
-						) : (
-							<FormControl.HelperText>{null}</FormControl.HelperText>
-						)}
-					</FormControl>
+								{'email' in errors ? (
+									<HStack marginTop='2'>
+										<AntDesign
+											name='exclamationcircleo'
+											size='xs'
+											color={red}
+										/>
+										<Text color={red}> {errors.email}</Text>
+									</HStack>
+								) : (
+									<FormControl.HelperText>{null}</FormControl.HelperText>
+								)}
+							</FormControl>
 
-					<FormControl
-						className={'PasswordContainer'}
-						isRequired
-						// isInvalid={'password' in errors}
-					>
-						<FormControl.Label>
-							<Text size='md'> Password</Text>
-						</FormControl.Label>
+							<FormControl className={'PasswordContainer'} isRequired>
+								<FormControl.Label>
+									<Text size='md'> Password</Text>
+								</FormControl.Label>
 
-						<Input
-							variant='filled'
-							rounded='10'
-							_focus={{ bg: white }}
-							type={show1 ? 'text' : 'password'}
-							InputRightElement={
-								<Pressable onPress={() => setShow1(!show1)}>
-									<Icon
-										as={
-											<MaterialIcons
-												name={show1 ? 'visibility' : 'visibility-off'}
+								<Input
+									variant='filled'
+									rounded='10'
+									_focus={{ bg: white }}
+									type={show1 ? 'text' : 'password'}
+									InputRightElement={
+										<Pressable onPress={() => setShow1(!show1)}>
+											<Icon
+												as={
+													<MaterialIcons
+														name={
+															show1 ? 'visibility' : 'visibility-off'
+														}
+													/>
+												}
+												size={5}
+												mr='2'
+												color='muted.400'
 											/>
-										}
-										size={5}
-										mr='2'
-										color='muted.400'
-									/>
-								</Pressable>
-							}
-							onChangeText={(value) => setData({ ...regData, password: value })}
-						/>
+										</Pressable>
+									}
+									onChangeText={(value) =>
+										setData({ ...regData, password: value })
+									}
+								/>
 
-						{regData.password !== undefined ? (
-							<VStack className={'ErrorContainerWrong'} marginTop='2'>
-								<Text> Password must contain</Text>
-								{regData.password.length < 6 || regData.password.length > 15 ? (
-									<HStack>
-										<AntDesign name='closecircle' size='xs' color={red} />
-										<Text color={red}> 6 to 16 characters</Text>
+								{regData.password !== undefined ? (
+									<VStack className={'ErrorContainerWrong'} marginTop='2'>
+										<Text> Password must contain</Text>
+										{regData.password.length < 6 ||
+										regData.password.length > 15 ? (
+											<HStack>
+												<AntDesign
+													name='closecircle'
+													size='xs'
+													color={red}
+												/>
+												<Text color={red}> 6 to 16 characters</Text>
+											</HStack>
+										) : (
+											<HStack>
+												<AntDesign
+													name='checkcircle'
+													size='xs'
+													color={green}
+												/>
+												<Text color={green}> 6 to 16 characters</Text>
+											</HStack>
+										)}
+
+										{!regexPassword.test(regData.password) ? (
+											<HStack>
+												<AntDesign
+													name='closecircle'
+													size='xs'
+													color={red}
+												/>
+												<Text color={red}> Only numbers and letters</Text>
+											</HStack>
+										) : (
+											<HStack>
+												<AntDesign
+													name='checkcircle'
+													size='xs'
+													color={green}
+												/>
+												<Text color={green}> Only numbers and letters</Text>
+											</HStack>
+										)}
+									</VStack>
+								) : 'password' in errors ? (
+									<HStack className={'ErrorContainerUndefined'} marginTop='2'>
+										<AntDesign
+											name='exclamationcircleo'
+											size='xs'
+											color={red}
+										/>
+										<Text color={red}> Password is mandatory!</Text>
 									</HStack>
-								) : (
-									<HStack>
-										<AntDesign name='checkcircle' size='xs' color={green} />
-										<Text color={green}> 6 to 16 characters</Text>
-									</HStack>
-								)}
+								) : null}
+							</FormControl>
 
-								{!regexPassword.test(regData.password) ? (
-									<HStack>
-										<AntDesign name='closecircle' size='xs' color={red} />
-										<Text color={red}> Only numbers and letters</Text>
-									</HStack>
-								) : (
-									<HStack>
-										<AntDesign name='checkcircle' size='xs' color={green} />
-										<Text color={green}> Only numbers and letters</Text>
-									</HStack>
-								)}
-							</VStack>
-						) : 'password' in errors ? (
-							<HStack className={'ErrorContainerUndefined'} marginTop='2'>
-								<AntDesign name='exclamationcircleo' size='xs' color={red} />
-								<Text color={red}> Password is mandatory!</Text>
-							</HStack>
-						) : null}
+							<FormControl className={'ConfirmPasswordContainer'} isRequired>
+								<FormControl.Label>
+									<Text size='md'> Confirm password</Text>
+								</FormControl.Label>
 
-						{/* In order to use validate function
-						{'password' in errors ? (
-							<FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size='xs' />}>
-								{errors.password}
-							</FormControl.ErrorMessage>
-						) : (
-							<FormControl.HelperText>{null}</FormControl.HelperText>
-						)}
-						*/}
-					</FormControl>
-
-					<FormControl
-						className={'ConfirmPasswordContainer'}
-						isRequired
-						// isInvalid={'password2' in errors}
-					>
-						<FormControl.Label>
-							<Text size='md'> Confirm password</Text>
-						</FormControl.Label>
-
-						<Input
-							variant='filled'
-							rounded='10'
-							_focus={{ bg: white }}
-							type={show2 ? 'text' : 'password'}
-							InputRightElement={
-								<Pressable onPress={() => setShow2(!show2)}>
-									<Icon
-										as={
-											<MaterialIcons
-												name={show2 ? 'visibility' : 'visibility-off'}
+								<Input
+									variant='filled'
+									rounded='10'
+									_focus={{ bg: white }}
+									type={show2 ? 'text' : 'password'}
+									InputRightElement={
+										<Pressable onPress={() => setShow2(!show2)}>
+											<Icon
+												as={
+													<MaterialIcons
+														name={
+															show2 ? 'visibility' : 'visibility-off'
+														}
+													/>
+												}
+												size={5}
+												mr='2'
+												color='muted.400'
 											/>
-										}
-										size={5}
-										mr='2'
-										color='muted.400'
-									/>
-								</Pressable>
-							}
-							onChangeText={(value) => setData({ ...regData, password2: value })}
-						/>
+										</Pressable>
+									}
+									onChangeText={(value) =>
+										setData({ ...regData, password2: value })
+									}
+								/>
 
-						{regData.password2 !== undefined ? (
-							<VStack className={'ErrorContainerUnmatched'} marginTop='2'>
-								{regData.password2 !== regData.password ? (
-									<HStack>
-										<AntDesign name='closecircle' size='xs' color={red} />
-										<Text color={red}> Passwords do not match</Text>
+								{regData.password2 !== undefined ? (
+									<VStack className={'ErrorContainerUnmatched'} marginTop='2'>
+										{regData.password2 !== regData.password ? (
+											<HStack>
+												<AntDesign
+													name='closecircle'
+													size='xs'
+													color={red}
+												/>
+												<Text color={red}> Passwords do not match</Text>
+											</HStack>
+										) : (
+											<HStack>
+												<Fontisto name='smiley' size='xs' color={green} />
+												{/* eslint-disable-next-line react/no-unescaped-entities */}
+												<Text color={green}> It's a match!</Text>
+											</HStack>
+										)}
+									</VStack>
+								) : 'password2' in errors ? (
+									<HStack className={'ErrorContainerUndefined'} marginTop='2'>
+										<AntDesign
+											name='exclamationcircleo'
+											size='xs'
+											color={red}
+										/>
+										<Text color={red}> Please confirm your password</Text>
+									</HStack>
+								) : null}
+							</FormControl>
+
+							<FormControl
+								className={'DegreeContainer'}
+								isInvalid={'degree' in errors}
+							>
+								<FormControl.Label>
+									<Text size='md'> Degree</Text>
+								</FormControl.Label>
+
+								<Input
+									variant='filled'
+									rounded='10'
+									_focus={{ bg: white }}
+									onChangeText={(value) => setData({ ...regData, degree: value })}
+								/>
+
+								{'degree' in errors ? (
+									<HStack marginTop='2'>
+										<AntDesign
+											name='exclamationcircleo'
+											size='xs'
+											color={red}
+										/>
+										<Text color={red}> {errors.degree}</Text>
 									</HStack>
 								) : (
-									<HStack>
-										<Fontisto name='smiley' size='xs' color={green} />
-										{/* eslint-disable-next-line react/no-unescaped-entities */}
-										<Text color={green}> It's a match!</Text>
-									</HStack>
+									<FormControl.HelperText>{null}</FormControl.HelperText>
 								)}
+							</FormControl>
+
+							<FormControl
+								className={'UniversityContainer'}
+								isInvalid={'university' in errors}
+							>
+								<FormControl.Label>
+									<Text size='md'> University</Text>
+								</FormControl.Label>
+
+								<Input
+									variant='filled'
+									rounded='10'
+									_focus={{ bg: white }}
+									onChangeText={(value) =>
+										setData({ ...regData, university: value })
+									}
+								/>
+
+								{'university' in errors ? (
+									<HStack marginTop='2'>
+										<AntDesign
+											name='exclamationcircleo'
+											size='xs'
+											color={red}
+										/>
+										<Text color={red}> {errors.university}</Text>
+									</HStack>
+								) : (
+									<FormControl.HelperText>{null}</FormControl.HelperText>
+								)}
+							</FormControl>
+
+							<Column pt='10%' space={5}>
+								<Button alignSelf='center' variant='alternating' onPress={validate}>
+									Register
+								</Button>
+
+								<Center>
+									<Link to={'/'} style={{ textDecoration: 'none' }}>
+										<Text size='md'>Cancel</Text>
+									</Link>
+								</Center>
+							</Column>
+						</Box>
+					) : (
+						<Box className={'Validated'}>
+							{/* eslint-disable-next-line react/no-unescaped-entities */}
+							<Heading>Congrats, you're in!</Heading>
+							<VStack pt='10%' space={5}>
+								<Link to={'/Login'} style={{ textDecoration: 'none' }}>
+									<Button alignSelf='center' variant='alternating'>
+										Login
+									</Button>
+								</Link>
+
+								{/*// TODO: SMALLER BUTTON*/}
+
+								<Center>
+									<Link to={'/'} style={{ textDecoration: 'none' }}>
+										<Text size='md'>Back to Home Page</Text>
+									</Link>
+								</Center>
 							</VStack>
-						) : 'password2' in errors ? (
-							<HStack className={'ErrorContainerUndefined'} marginTop='2'>
-								<AntDesign name='exclamationcircleo' size='xs' color={red} />
-								<Text color={red}> Please confirm your password</Text>
-							</HStack>
-						) : null}
-
-						{/* In order to use validate function
-						{'password2' in errors ? (
-							<FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size='xs' />}>
-								{errors.password2}
-							</FormControl.ErrorMessage>
-						) : (
-							<FormControl.HelperText>{null}</FormControl.HelperText>
-						)}
-						*/}
-					</FormControl>
-
-					<FormControl className={'DegreeContainer'} isInvalid={'degree' in errors}>
-						<FormControl.Label>
-							<Text size='md'> Degree</Text>
-						</FormControl.Label>
-
-						<Input
-							variant='filled'
-							rounded='10'
-							_focus={{ bg: white }}
-							onChangeText={(value) => setData({ ...regData, degree: value })}
-						/>
-
-						{'degree' in errors ? (
-							<HStack marginTop='2'>
-								<AntDesign name='exclamationcircleo' size='xs' color={red} />
-								<Text color={red}> {errors.degree}</Text>
-							</HStack>
-						) : (
-							<FormControl.HelperText>{null}</FormControl.HelperText>
-						)}
-					</FormControl>
-
-					<FormControl
-						className={'UniversityContainer'}
-						isInvalid={'university' in errors}
-					>
-						<FormControl.Label>
-							<Text size='md'> University</Text>
-						</FormControl.Label>
-
-						<Input
-							variant='filled'
-							rounded='10'
-							_focus={{ bg: white }}
-							onChangeText={(value) => setData({ ...regData, university: value })}
-						/>
-
-						{'university' in errors ? (
-							<HStack marginTop='2'>
-								<AntDesign name='exclamationcircleo' size='xs' color={red} />
-								<Text color={red}> {errors.university}</Text>
-							</HStack>
-						) : (
-							<FormControl.HelperText>{null}</FormControl.HelperText>
-						)}
-					</FormControl>
-
-					<Column pt='10%' space={5}>
-						<Button alignSelf='center' variant='alternating' onPress={validate}>
-							Register
-						</Button>
-
-						<Center>
-							<Link to={'/'} style={{ textDecoration: 'none' }}>
-								<Text size='md'>Cancel</Text>
-							</Link>
-						</Center>
-					</Column>
+						</Box>
+					)}
 				</StyledBox>
 			</Row>
 		</Column>
